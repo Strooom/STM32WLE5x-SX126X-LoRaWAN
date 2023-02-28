90f670a822a8109b71f4c469fd704fd0b39a7655
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2023-02-28 20:20:30 +0100

    extend nvs library with I2C access

--- src/general/nvs.cpp
+++ src/general/nvs.cpp
@@ -6,23 +6,42 @@
 
 #include "nvs.h"
 
-void nonVolatileStorage::read(uint32_t blockIndex, uint8_t* destinationDataBuffer) {
-    if (blockIndex < theNvsMap.numberOfBlocksUsed) {
-        uint32_t startAddress = theNvsMap.block[blockIndex].startAddress;
-        uint32_t length       = theNvsMap.block[blockIndex].length;
-        for (uint32_t i = 0; i < length; i++) {
-            // destinationDataBuffer[i] = EEPROM.read(startAddress + i);
-        }
+bool isReady() {
+    return (HAL_OK == HAL_I2C_IsDeviceReady(I2C_HandleTypeDef * hi2c, uint16_t DevAddress, uint32_t Trials, uint32_t Timeout));
+}
+
+uint8_t nonVolatileStorage::read8(uint32_t blockIndex) {
+    if (isValidBlockIndex(blockIndex)) {
+        uint16_t MemAddress = static_cast<uint16_t>(theNvsMap.block[blockIndex].startAddress);
+        uint16_t Size       = static_cast<uint16_t>(theNvsMap.block[blockIndex].length);
+        uint8_t result;
+        HAL_I2C_Mem_Read(I2C_HandleTypeDef * hi2c, 0xA0, MemAddress, 2U, &result, Size, 50U);
     } else {
-        // error : blockIndex out of range
+        // TODO : log this error
+        return 0;
     }
 }
 
-void nonVolatileStorage::write(uint32_t blockIndex, uint8_t* sourceDataBuffer) {
-    if (blockIndex < theNvsMap.numberOfBlocksUsed) {
+uint16_t nonVolatileStorage::read16(uint32_t blockIndex) {}
+uint32_t nonVolatileStorage::read32(uint32_t blockIndex) {}
+uint64_t nonVolatileStorage::read64(uint32_t blockIndex) {}
+aesKey nonVolatileStorage::readAesKey(uint32_t blockIndex) {}
+
+void nonVolatileStorage::write(uint32_t blockIndex, uint8_t sourceData) {
+    if (isValidBlockIndex(blockIndex)) {
+        uint16_t MemAddress = static_cast<uint16_t>(theNvsMap.block[blockIndex].startAddress);
+        uint16_t Size       = static_cast<uint16_t>(theNvsMap.block[blockIndex].length);
+
+        HAL_I2C_Mem_Write(I2C_HandleTypeDef * hi2c, 0xA0, MemAddress, 2U, &sourceData, Size, 50U);
     } else {
+        // TODO : log this error
     }
 }
+
+void nonVolatileStorage::write(uint32_t blockIndex, uint16_t sourceData) {}
+void nonVolatileStorage::write(uint32_t blockIndex, uint32_t sourceData) {}
+void nonVolatileStorage::write(uint32_t blockIndex, uint64_t sourceData) {}
+void nonVolatileStorage::write(uint32_t blockIndex, aesKey sourceData) {}
 
 uint32_t nvsMap::addBlock(uint32_t length) {
     // TODO : check maximimum size of map 1K
@@ -39,11 +58,14 @@ uint32_t nvsMap::addBlock(uint32_t length) {
     }
 }
 
-
 uint32_t nonVolatileStorage::getNumberOfBlocksUsed() const {
     return theNvsMap.numberOfBlocksUsed;
 }
 
 uint32_t nonVolatileStorage::getNumberOfBytesUsed() const {
     return theNvsMap.nextStartAddress;
+}
+
+bool nonVolatileStorage::isValidBlockIndex(uint32_t blockIndex) const {
+    return (blockIndex < theNvsMap.numberOfBlocksUsed);
 }
--- src/general/nvs.h
+++ src/general/nvs.h
@@ -6,6 +6,7 @@
 
 #pragma once
 #include <stdint.h>
+#include "aeskey.h"
 
 // an nvsBlock is a region of non-volatile storage in external EEPROM
 // this region is defined by a start address and a length
@@ -32,43 +33,53 @@ class nvsMap {
     uint32_t numberOfBlocksUsed{0};
     uint32_t nextStartAddress{0};
 
+    // Required Persistent Values for ABP Devcies In the event of a reset or loss of power, the following values must be persisted so that an ABP end device can return to the network connection session where it left off before the loss of power:
+    // DevEUI
+    // DevAddr
+    // SessionKeys
+    // Frame Counters (both uplink and downlink)
+    // Channel List (frequencies and enabled channels)
+    // Data rate
+    // TXPower
+    // NbTrans
+    // MaxDutyCycle
+    // RX2Frequency
+    // RX1DROffset
+    // RX2DataRate
+    // RXTimingDelay
+    // MaxEIRP
+    // DownlinkDwellTime
+    // UplinkDwellTime
 
-        // Required Persistent Values for ABP Devcies In the event of a reset or loss of power, the following values must be persisted so that an ABP end device can return to the network connection session where it left off before the loss of power:
-        // DevEUI
-        // DevAddr
-        // SessionKeys
-        // Frame Counters (both uplink and downlink)
-        // Channel List (frequencies and enabled channels)
-        // Data rate
-        // TXPower
-        // NbTrans
-        // MaxDutyCycle
-        // RX2Frequency
-        // RX1DROffset
-        // RX2DataRate
-        // RXTimingDelay
-        // MaxEIRP
-        // DownlinkDwellTime
-        // UplinkDwellTime
-
-        // Note for OTAA there is some delta
-        // not stored : DevAddr & Session Keys
-        // To be stored :
-        // JoinEUI
-        // Root Keys AppKey / NwkKey
-        // JoinNonce
-
-    };
-
+    // Note for OTAA there is some delta
+    // not stored : DevAddr & Session Keys
+    // To be stored :
+    // JoinEUI
+    // Root Keys AppKey / NwkKey
+    // JoinNonce
+};
 
 class nonVolatileStorage {
   public:
-    void read(uint32_t blockIndex, uint8_t* destinationDataBuffer);        // reads the block from EEPROM and stores it in the destinationDataBuffer
-    void write(uint32_t blockIndex, uint8_t* sourceDataBuffer);            // write data from sourceDataBuffer to the block in EEPROM
-    uint32_t addBlock(uint32_t length);                                    // adds a block to the nvsMap, returns the blockIndex
+    bool isReady();
+
+    uint8_t read8(uint32_t blockIndex);
+    uint16_t read16(uint32_t blockIndex);
+    uint32_t read32(uint32_t blockIndex);
+    uint64_t read64(uint32_t blockIndex);
+    aesKey readAesKey(uint32_t blockIndex);
+
+    void write(uint32_t blockIndex, uint8_t sourceData);
+    void write(uint32_t blockIndex, uint16_t sourceData);
+    void write(uint32_t blockIndex, uint32_t sourceData);
+    void write(uint32_t blockIndex, uint64_t sourceData);
+    void write(uint32_t blockIndex, aesKey sourceData);
+
+    uint32_t addBlock(uint32_t length);        // adds a block to the nvsMap, returns the blockIndex
     uint32_t getNumberOfBlocksUsed() const;
     uint32_t getNumberOfBytesUsed() const;
 
   private:
     nvsMap theNvsMap;
+    bool isValidBlockIndex(uint32_t blockIndex) const;
 };
